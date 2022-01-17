425ba82c043085f6f8825f3508182ae2250ac097
jsantamariaBF <jorge.santamaria@blankfactor.com>
2022-01-17 00:16:32 -0500

    first commit

---
+++ src/includes/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA6wVvhWevVPQp63K8xrgwJhTe5nl4Y6HY',
+  authDomain: 'music-dc876.firebaseapp.com',
+  projectId: 'music-dc876',
+  storageBucket: 'music-dc876.appspot.com',
+  appId: '1:909064388332:web:aa8eb7d54fd87d486a6fc9',
+  measurementId: 'G-373NZDX3PK',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const usersCollection = db.collection('users');
+
+export {
+  auth,
+  db,
+  usersCollection,
+};
