2b4ef05540bdc5e61f2c6e35d1e91cd2959da1dd
jsantamariaBF <jorge.santamaria@blankfactor.com>
2022-01-24 08:05:04 -0500

    fixing auth

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -80,7 +80,7 @@ export default {
   },
   methods: {
     ...mapMutations(['toggleAuthModal']),
-    register(values) {
+    register() {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = 'bg-blue-500';
@@ -88,7 +88,6 @@ export default {
 
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created.';
-      console.log(values);
     },
   },
 };
